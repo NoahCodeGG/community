5d9e42ffc88bd55e7e0171000f317102a2c6b70e
NoahCode <noahcode@aliyum.com>
2019-12-01 09:34:08 +0800

    session judge button and information

--- src/main/java/cn/noahcode/community/controller/AuthorizeController.java
+++ src/main/java/cn/noahcode/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author NoahCode
  * @date 2019/11/30
@@ -30,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                                   HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -39,7 +42,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
     }
 }
