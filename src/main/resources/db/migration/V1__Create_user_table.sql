dc1f12fcc83898c431c72eeab27e9c4dace1d4c6
NoahCode <31448330+NoahCodeMonkey@users.noreply.github.com>
2019-12-02 09:49:42 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,11 @@
+create table USER
+(
+    ID           INTEGER auto_increment,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+    constraint TABLE_NAME_PK
+        primary key (ID)
+);
